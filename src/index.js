5afe906cfdfeacd4adc34589823ccb08eb7f2d3b
Mateusz Kuchta <mtkuchta@gmail.com>
2020-10-11 11:04:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import {
+//     message,
+//     messageDOM
+// } from './tools/message';
+// import './components/footer'
+// import info from './data/title.txt';
+import './sass/index.scss';
+// import addImage from './tools/image';
+// import Creator from './tools/creator';
+
+console.log('bangla')
