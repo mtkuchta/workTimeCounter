d7ab828e7860de619977d68812d71b07ec9b4ce1
Mateusz Kuchta <mtkuchta@gmail.com>
2020-10-13 18:41:46 +0200

    improve js projects-menu and add localStorage

--- src/components/projects-menu.js
+++ src/components/projects-menu.js
@@ -1,39 +1,59 @@
  class ProjectsMenu{
     constructor(){
         this.projectsList = document.querySelector('.project-menu__list');
+        this.projects=JSON.parse(localStorage.getItem('projects')) || [];
+        this.populateProjects(this.projects,this.projectsList);
     }
 
     addProject(e, newProject){
         e.preventDefault()
-       
-        const project = document.createElement('li');
         const input = document.querySelector('.projects-menu__input-project')
+        const project = {
+            name: newProject,
+            dataIndex: this.projects.length ,
+            tasks:[],
+            isActive:false,
+        };
 
         input.value= '';
-        project.setAttribute('class', 'projects-menu__project project');
-        project.innerHTML=
-        `
-        <h2 class="project__title">${newProject}</h2>
-        <span class="fas fa-caret-down expand"></span>
-        <div class="project__container">
-                <ul class="project__task-list">
-                  
-                </ul>
-                <form class="project__form">
-                  <input type="text" class="project__newTaskInp" name="newTask" placeholder="Nowe zadanie..." required>
-                  <button class="project__button">Dodaj zadanie</button>
-                </form>
-              </div>
-        `
-        this.projectsList.appendChild(project)
-        this.addListenersToProject(project);
-   
+        this.projects.push(project)
+        //add to localStorage
+        localStorage.setItem('projects', JSON.stringify(this.projects));
+
+        this.populateProjects(this.projects, this.projectsList)
+        
+    }
+
+    populateProjects(projects =[], projectsList){
+        projectsList.textContent='';
+        projects.forEach((project)=>{
+            const newProject = document.createElement('li');
+            newProject.setAttribute('class', 'projects-menu__project project');
+            newProject.setAttribute('data-index', `${project.dataIndex}`)
+            newProject.innerHTML =
+            `
+            <h2 class="project__title">${project.name}</h2>
+             <span class="fas fa-caret-down expand ${project.isActive ===true? 'open': '' }"></span>
+             <div class="project__container ${project.isActive ===true? 'open': '' }" data-index="${project.dataIndex}">
+                     <ul class="project__task-list">
+                      
+                     </ul>
+                    <form class="project__form">
+                       <input type="text" class="project__newTaskInp" name="newTask" placeholder="Nowe zadanie..." required>
+                       <button class="project__button">Dodaj zadanie</button>
+                     </form>
+                   </div>
+            `
+            projectsList.appendChild(newProject)
+            this.addListenersToProject(newProject)
+            this.populateTasks(this.projects)
+        })
+
     }
 
     addListenersToProject(project){
         const expandBtn = project.querySelector('.expand');
         const addTaskForm = project.querySelector('.project__form');
-        console.log(expandBtn,addTaskForm)
         expandBtn.addEventListener('click', (e)=>this.showTasks(e));
         addTaskForm.addEventListener('submit', (e)=>this.addTask(
             e,
@@ -43,6 +63,14 @@
 
     showTasks(e){
         const btn = e.target;
+        const projectIndex =e.target.parentElement.dataset.index;
+
+        if(this.projects[projectIndex].isActive === false){
+            this.projects[projectIndex].isActive=true;
+        }else{
+            this.projects[projectIndex].isActive=false;
+        }
+
         const parent = btn.parentElement;
         const divContainer = parent.querySelector('.project__container');
         btn.classList.toggle('open')
@@ -52,22 +80,35 @@
 
     addTask(e,newTask){
         e.preventDefault()
-        const input = e.target;
-        input.querySelector('.project__newTaskInp').value='';
-        
-        const li = document.createElement('li');
-        li.classList.add('project__task');
-        li.innerHTML=
-        `
-        <h3 class="project__task-title">${newTask}</h3>
-        <span class="fas fa-user-clock add"></span>
-        `
+        const projectIndex = e.target.parentElement.dataset.index;
+        this.projects[projectIndex].tasks.push(newTask);
 
-        const divContainer = e.target.parentElement;
-        const taskList = divContainer.querySelector('ul')
-     
-        taskList.appendChild(li);
-        console.log(e.target.parentElement)
+        const input = e.target;
+        input.querySelector('.project__newTaskInp').value=''; 
+        //add to local storage
+        localStorage.setItem('projects', JSON.stringify(this.projects));
+        this.populateProjects(this.projects, this.projectsList)
+
+        
+    }
+
+    populateTasks(projects=[]){
+
+        const projectsElements = document.querySelectorAll('.project')
+        projectsElements.forEach((item,index)=>{
+            const ul = item.querySelector('ul');
+            ul.textContent='';
+            projects[index].tasks.forEach(task=>{
+                const li = document.createElement('li');
+                li.classList.add('project__task');
+                li.innerHTML=
+                `
+                <h3 class="project__task-title">${task}</h3>
+                <span class="fas fa-user-clock add"></span>
+                    `
+                 ul.appendChild(li)
+            });       
+        });
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@
 //     messageDOM
 // } from './tools/message';
 import {ProjectsMenu} from './components/projects-menu.js'
-// import info from './data/title.txt';
 import './sass/index.scss';
-// import addImage from './tools/image';
-// import Creator from './tools/creator';
 
 const projectsMenu = new ProjectsMenu();
-
 
 const addProjectForm = document.querySelector('.projects-menu__addForm');
 addProjectForm.addEventListener('submit', (e)=>projectsMenu.addProject(
